2b07956ca7990803f09635f3ae0fe8f7bc83248b
shwetabhmanas <75967185+shwetabhmanas@users.noreply.github.com>
2020-12-14 11:10:25 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd6f94bd84ef0001e94b901",
+				"5fd6f94bd84ef0001e94b903"
+			]
+		},
+		"5fd6f94bd84ef0001e94b901": {
+			"id": "5fd6f94bd84ef0001e94b901",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd6f94bd84ef0001e94b903": {
+			"id": "5fd6f94bd84ef0001e94b903",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
